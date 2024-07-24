85a50c750b1650e08efcc3286f02cc29c7067e0b
KeeratiNon <keeratinon3@gmail.com>
2024-07-24 14:34:10 +0700

    answer ex1-2

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,18 @@
+const customers = ["Somchai", "Arthit", "Suchada", "Napasorn", "Pimchanok"]
+
 function sortCustomerName(customers) {
   // เริ่มเขียนโค้ดตรงนี้จ้า
+  for (let i = 0; i < customers.length - 1; i++) {
+    for ( let j = 0; j < customers.length - 1 - i; j++) {
+      if (customers[j] > customers[j+1]) {
+        let curr = customers[j]
+        customers[j] = customers[j+1]
+        customers[j+1] = curr
+      }
+    }
+  }
+  return customers
 }
 
 // ตอบคำถามตรงนี้จ้า
+console.log(sortCustomerName(customers))
